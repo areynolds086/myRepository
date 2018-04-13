54fbc2a135470c5fe68c8575ab40066abc5b07cf
Aaron Reynolds <areynolds8@mail.greenriver.edu>
2018-04-12 19:52:13 -0700

    Added code in hello.php to make it HTML5 valid

--- hello.php
+++ hello.php
@@ -1,2 +1,13 @@
-<?php
-  echo 'Hello World!';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo '<p>Hello World!</p>';
+    ?>
+
+  </body>
+</html>
